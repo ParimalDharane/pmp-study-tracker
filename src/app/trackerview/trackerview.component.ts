25a6144bf4ed31fbd05b2408fbfafb27c39d3dd4
Parimal Dharane <myemail@gmail.com>
2019-07-03 18:07:34 +0530

    implemented data retrival for usertopics

--- src/app/trackerview/trackerview.component.ts
+++ src/app/trackerview/trackerview.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { UsertopicService } from 'src/app/shared/usertopic.service';
+import { Usertopic } from 'src/app/shared/usertopic.model';
 
 @Component({
   selector: 'app-trackerview',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrackerviewComponent implements OnInit {
 
-  constructor() { }
+  defDateFormat:'yyyy/MM/dd h:mm:ss a';
+  usertopicList: Usertopic[];
 
-  ngOnInit() {
-  }
+  constructor(private service: UsertopicService,
+    private firestore: AngularFirestore) { }
 
+    ngOnInit() {
+      this.service.getUsertopics().subscribe(actionArray => {
+        this.usertopicList = actionArray.map(item => {
+          return {
+            topicID: item.payload.doc.id,
+            ...item.payload.doc.data()
+          } as Usertopic;
+        })
+      });
+    }
+
+    onEdit(usertopicObj: Usertopic) {
+      this.service.formData = Object.assign({}, usertopicObj);
+    }
+   
+    onDelete(topicID: string) {
+      if (confirm("Are you sure to delete this record?")) {
+        this.firestore.doc('usertopic/' + topicID).delete();
+      }
+    }
+  
 }
